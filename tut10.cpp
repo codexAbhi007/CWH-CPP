4b6867bb2c9bef89fe56bc58a9ea8073917fe25e
codexAbhi007 <codewithabhi2024@gmail.com>
2025-01-06 19:20:32 +0530

    initial commit

---
+++ tut10.cpp
@@ -0,0 +1,9 @@
+// Author: Abhi
+// Date: 06/01/2025
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
---
+++ tut9.cpp
@@ -0,0 +1,15 @@
+//#C++ control structures
+//!1)BASIC CONTROL STRUCTURE
+//~> SEQUENCE STRUCTURE 
+//~> SELECTION STRUCTURE 
+//~> LOOP STRUCTURE 
+
+
+//#C++ TYPES OF DATA TYPES
+//!1)PRIMITIVE DATA TYPES
+//~> int,float,double,char,bool
+//!2)DERIVED DATA TYPES
+//~> pointer,array,function,reference
+//!3)USER DEFINED DATA TYPES
+//~> struct, class, enum, typedef/using, union
+
